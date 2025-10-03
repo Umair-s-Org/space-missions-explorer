75e6fe5c51c36b1367b423d2d445349eed5aaf5a
Umair Zafar <umair2406@gmail.com>
2025-10-03 18:35:13 +0000

    remove the condition to run the server only if directly called by the filename

--- app.js
+++ app.js
@@ -80,11 +80,8 @@ app.get('/ready',   function(req, res) {
     });
 })
 
-if (require.main === module) {
-    app.listen(3000, () => {
+app.listen(3000, () => {
         console.log("Server successfully running on port - 3000");
     });
-}
-
 
 module.exports = app;
